2c63dd0f7667f9319b766f75a380c7d09afcc4bb
Nathan Gunzenhauser <dracomancer13@yahoo.com>
2015-11-04 12:29:38 -0500

    Update Citizen.java
    Moved static getCitizen() method from Util class to Citizen class

--- src/main/java/io/github/griffenx/CityZen/Citizen.java
+++ src/main/java/io/github/griffenx/CityZen/Citizen.java
@@ -23,6 +23,32 @@ public class Citizen {
 		}
 	}
 	
+	/**
+	 * Gets a Citizen by name from list of citizens in memory. 
+	 * If somehow that player is online, but their citizen record is not in memory, it will be added.
+	 * This method should be used exclusively for getting citizens, NOT initializing a new Citizen.
+	 * @param name
+	 * The name of the player to get
+	 * @return
+	 * Returns a citizen from memory if online, or a new citizen record if offline.
+	 */
+	public static Citizen getCitizen(UUID uuid) {
+		Citizen ctz = null;
+		for(Citizen c : CityZen.citizens) {
+			//TODO: Get player's UUID (not quite sure how to do this w/o docs)
+			if (c.passport.getName().equalsIgnoreCase(name)) {
+				ctz = c;
+			}
+		}
+		
+		if (ctz == null) {
+			Player newCtz = CityZen.getPlugin().getServer().getPlayer(UUID.fromString(name));
+			ctz = new Citizen(newCtz);
+			if (newCtz.isOnline()) CityZen.citizens.add(ctz);
+		}
+		return ctz;
+	}
+	
 	public void subRep(int amount) {
 		reputation -= amount;
 		fixRep();
