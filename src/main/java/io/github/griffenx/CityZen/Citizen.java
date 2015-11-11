be861d009a10674b08a829ee1d5e82c1e0884385
Nathan Gunzenhauser <dracomancer13@yahoo.com>
2015-11-11 11:37:07 -0500

    Update Citizen.java
    Basic methods in place.

--- src/main/java/io/github/griffenx/CityZen/Citizen.java
+++ src/main/java/io/github/griffenx/CityZen/Citizen.java
@@ -13,38 +13,74 @@ public class Citizen {
 		this((Player) CityZen.getPlugin().getServer().getOfflinePlayer(uuid));
 	}
 	public Citizen(Player player) {
-		properties = CityZen.citizenConfig.getConfig().getConfigurationSection("citizens." + player.getUniqueId().toString());
-		
-		//TODO: write setter methods for passport and alerts
-		//passport = player;
+		FileConfiguration citizenConfig = CityZen.citizenConfig.getConfig();
+		if (!citizenConfig.contains("citizens." + player.getUniqueId().toString())) {
+			citizenConfig.createSection("citizens." + player.getUniqueId().toString()));
+		}
+		properties = citizenConfig.getConfigurationSection("citizens." + player.getUniqueId().toString());
 		
 		//alerts = properties.getStringList("alerts");
 	}
 	
 	/**
-	 * Gets a Citizen by name from list of citizens in memory. 
-	 * If somehow that player is online, but their citizen record is not in memory, it will be added.
-	 * This method should be used exclusively for getting online citizens, NOT initializing a new Citizen.
-	 * @param uuid
-	 * The uuid of the player to get
-	 * @return
-	 * Returns a citizen from memory if online, or a new citizen record if offline.
+	 * Creates and initializes a new Citizen.
+	 * @param player The player for whom to create a Citizen record
+	 * @returns
+	 * A new Citizen record for this player. Returns null if the specified player already has a Citizen record.
 	 */
-	public static Citizen getCitizen(UUID uuid) {
+	public static Citizen createCitizen(Player player) {
 		Citizen ctz = null;
-		for(Citizen c : CityZen.citizens) {
-			if (c.getPassport().getUniqueId().equals(uuid)) {
-				ctz = c;
-			}
+		for (Citizen c : getCitizens()) {
+			if (player.getUniqueId().equals(c.getPassport().getUniqueId())) return ctz;
 		}
-		if (ctz == null) {
-			Player newCtz = CityZen.getPlugin().getServer().getPlayer(uuid);
-			ctz = new Citizen(newCtz);
-			if (newCtz.isOnline()) CityZen.citizens.add(ctz);
-		}
+		ctz = new Citizen(player);
+		ctz.setReputation(CityZen.getPlugin().getConfig().getInt("reputation.default");
 		return ctz;
 	}
 	
+	public static void deleteCitizen(Citizen citizen) {
+		//TODO: Remove their configuration section
+		//All city/plot related stuff should be handled by the city
+	}
+	
+	/**
+	 * Gets a list of all Citizens on file.
+	 * @returns
+	 * A List containing all known Citizens
+	 */
+	public static List<Citizen> getCitizens() {
+		List<Citizen> citizens = new Vector<Citizen>();
+		ConfigurationSection configSection = CityZen.citizenConfig.getConfig().getConfigurationSection("citizens");
+		for (String c : configSection.getKeys(false)) {
+			citizens.add(new Citizen(UUID.fromString(c)));
+		}
+		return citizens;
+	}
+	
+	/**
+	 * Returns the username of this player from file. Used for display purposes.
+	 * @returns
+	 * The username of this Citizen
+	 */
+	public String getName() {
+		return getProperty("name");
+	}
+	
+	/**
+	 * Set the name of this player on file. Really only used for display purposes, and should be auto-updated when the player logs in.
+	 * @param newName The new name to assign to this Citizen
+	 */
+	public void setName(String newName) {
+		setProperty("name",newName);
+	}
+	
+	/**
+	 * Gets the reputation of this player.
+	 * If the reputation field in the config is not set properly, it sets it to -1.
+	 * This is essentially a check for improper config.
+	 * @returns
+	 * The reputation of this player
+	 */
 	public int getReputation() {
 		int rep;
 		try {
@@ -55,22 +91,51 @@ public class Citizen {
 		return rep;
 	}
 	
+	/**
+	 * Increases the reptuation of this player. Negative amounts are allowed.
+	 * Does not let the player's reputation drop below 0 or rise above Integer.MAX_VALUE
+	 * @param amount
+	 * The amount by which to increase this player's reputation
+	 */
 	public void addReputation(int amount) {
-		int rep = getReputation() + amount;
-		fixRep();
-		setProperty("reputation",rep);
+		if (amount > (Integer.MAX_VALUE - getReputation()) setReputation(Integer.MAX_VALUE - 1);
+		else {
+			int rep = getReputation() + amount;
+			fixRep();
+			setReputation(rep);
+		}
 	}
 	
+	/**
+	 * Subtract reputation from this player. Negative amounts are allowed.
+	 * Will not let the player's reputation drop below 0 or rise above Integer.MAX_VALUE
+	 * @param amount
+	 * The amount by which to decrease this player's reputation.
+	 */
 	public void subReputation(int amount) {
-		int rep = getReputation() - amount;
+		int rep;
+		if (amount < 0 && (Math.Abs(amount) > (Integer.MAX_VALUE - getReputation()))) rep = Integer.MAX_VALUE - 1;
+		else rep = getReputation() - amount;
 		fixRep();
 		setProperty("reputation",rep);
 	}
 	
+	/**
+	 * Sets the reputation of the player to a certain amount. 
+	 * Must be between 0 and the maximum value of a 32-bit signed Integer (a little over 2 billion)
+	 * @param amount
+	 * The amount of reputation to set to this player
+	 */
 	public void setReputation(int amount) {
 		if (amount >= 0 && amount < Integer.MAX_VALUE) setProperty("reputation",amount);
+		else setProperty("reputation",0);
 	}
 	
+	/**
+	 * Gets a City object of the city with which this player is affiliated
+	 * @returns
+	 * This player's city
+	 */
 	public City getAffiliation() {
 		City aff = null;
 		String affname = getProperty("affiliation");
@@ -81,7 +146,8 @@ public class Citizen {
 	}
 	
 	/**
-	 * Used to set which city this Citizen is affiliated with. DO NOT USE to add a player to a city.
+	 * Used to set which city this Citizen is affiliated with. Mostly for internal use.
+	 * DO NOT USE to add a player to a city.
 	 * You should instead call addCitizen() on the city.
 	 * @param city
 	 * The City to set as this player's affiliation
