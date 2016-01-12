abd321e57bd6929a7d9aedb1bf55d8dee5599de1
Nathan Gunzenhauser <dracomancer13@yahoo.com>
2016-01-12 16:23:22 -0500

    Update Reward.java
    Began work on rewards class

--- src/main/java/io/github/griffenx/CityZen/Reward.java
+++ src/main/java/io/github/griffenx/CityZen/Reward.java
@@ -1,5 +1,16 @@
 package io.github.griffenx.CityZen;
 
 public class Reward {
-
+    
+    private Config config = CityZen.rewardConfig.getConfig();
+    
+    private int id;
+    
+    public Reward(int id) {
+        this.id = id;
+    }
+    
+    private String[] readReward() {
+        
+    }
 }
