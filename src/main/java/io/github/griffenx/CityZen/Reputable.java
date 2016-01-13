0493b4efd5f0f73be59ba56512bc3afbc96b668a
Nathan Gunzenhauser <dracomancer13@yahoo.com>
2016-01-13 16:02:21 -0500

    Update Reputable.java
    Added additional method signatures.

--- src/main/java/io/github/griffenx/CityZen/Reputable.java
+++ src/main/java/io/github/griffenx/CityZen/Reputable.java
@@ -4,4 +4,8 @@ public interface Reputable {
     abstract long getReputation();
     
     abstract long getMaxReputation();
+    
+    abstract void sendReward(Reward r);
+    
+    abstract List<Reward> getRewards();
 }
