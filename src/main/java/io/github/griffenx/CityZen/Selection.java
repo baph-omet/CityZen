159dacef3f4c3c720ca58465090dceac1c818f52
Sarah Hawthorne <mail@iamvishnu.net>
2021-06-03 10:06:42 -0400

    Fixed issues with plots and WorldGuard API

--- src/main/java/io/github/griffenx/CityZen/Selection.java
+++ src/main/java/io/github/griffenx/CityZen/Selection.java
@@ -1,44 +1,50 @@
 package io.github.griffenx.CityZen;
 
-import com.sk89q.worldguard.LocalPlayer;
+import org.bukkit.Location;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.Flags;
-import com.sk89q.worldedit.extent.Extent;
-import com.sk89q.worldedit.util.Location;
 
 public class Selection {
 	public Position pos1;
 	public Position pos2;
-	
+
 	public Selection(Position pos1, Position pos2) {
-		if (!pos1.world.equals(pos2.world)) return;
+		if (!pos1.world.equals(pos2.world))
+			return;
 		this.pos1 = pos1;
 		this.pos2 = pos2;
 	}
-	
+
 	public Double getArea() {
-		return Math.abs((double)((pos1.x - pos2.x) * (pos1.z - pos2.z)));
+		return Math.abs((pos1.x - pos2.x) * (pos1.z - pos2.z));
 	}
-	
+
 	public Double getSideX() {
 		return Math.abs(pos1.x - pos2.x);
 	}
-	
+
 	public Double getSideZ() {
 		return Math.abs(pos1.z - pos2.z);
 	}
-	
+
 	public Selection getBuffer(double bufferAdjustment) {
-		double bufferSize = CityZen.getPlugin().getConfig().getDouble("plotBuffer") + bufferAdjustment;
-		if (bufferSize <= 0) return this;
-		Position bpos1 = new Position(pos1.world,pos1.x + (pos1.x < pos2.x ? -1 : 1) * bufferSize,pos1.y,pos1.z + (pos1.z < pos2.z ? -1 : 1) * bufferSize);
-		Position bpos2 = new Position(pos2.world,pos2.x + (pos1.x > pos2.x ? -1 : 1) * bufferSize,pos2.y,pos2.z + (pos1.z > pos2.z ? -1 : 1) * bufferSize);
-		return new Selection(bpos1,bpos2);
+		final double bufferSize = CityZen.getPlugin().getConfig().getDouble("plotBuffer") + bufferAdjustment;
+		if (bufferSize <= 0)
+			return this;
+		final Position bpos1 = new Position(pos1.world, pos1.x + (pos1.x < pos2.x ? -1 : 1) * bufferSize, pos1.y,
+				pos1.z + (pos1.z < pos2.z ? -1 : 1) * bufferSize);
+		final Position bpos2 = new Position(pos2.world, pos2.x + (pos1.x > pos2.x ? -1 : 1) * bufferSize, pos2.y,
+				pos2.z + (pos1.z > pos2.z ? -1 : 1) * bufferSize);
+		return new Selection(bpos1, bpos2);
 	}
+
 	public Selection getBuffer() {
 		return getBuffer(0);
 	}
-	
+
 	public boolean isSpaced(City affiliation) {
 		double xLow = pos1.x;
 		double xHigh = pos2.x;
@@ -52,19 +58,18 @@ public class Selection {
 			zLow = pos2.z;
 			zHigh = pos1.z;
 		}
-		
-		for (double x = xLow; x <= xHigh; x++) {
-			for (double z = zLow; z <= zHigh; z++) {
-				for (City c : City.getCities()) {
-					if (!c.equals(affiliation) && c.getCenter() != null) {
-						if (c.isInCity(x,z) || (c.getWorld().equals(pos1.world) 
-								&& Util.getDistace(new Position(pos1.world, x, 0, z), c.getCenter()) < CityZen.getPlugin().getConfig().getInt("minCitySeparation"))) return false;
-					}
-				}
-			}
-		} return true;
+
+		for (double x = xLow; x <= xHigh; x++)
+			for (double z = zLow; z <= zHigh; z++)
+				for (final City c : City.getCities())
+					if (!c.equals(affiliation) && c.getCenter() != null)
+						if (c.isInCity(x, z) || (c.getWorld().equals(pos1.world)
+								&& Util.getDistace(new Position(pos1.world, x, 0, z), c.getCenter()) < CityZen
+										.getPlugin().getConfig().getInt("minCitySeparation")))
+							return false;
+		return true;
 	}
-	
+
 	public boolean worldGuardConflicts(Citizen citizen) {
 		if (CityZen.WorldGuard != null) {
 			double xLow = pos1.x;
@@ -79,16 +84,15 @@ public class Selection {
 				zLow = pos2.z;
 				zHigh = pos1.z;
 			}
-			
-			for (double y = 1; y <= pos1.world.getMaxHeight(); y++) {
-				for (double x = xLow; x <= xHigh; x++) {
-					for (double z = zLow; z <= zHigh; z++) {
-						if (!WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery().testState(new Location((Extent) pos1.world, x, y, z), (LocalPlayer)citizen.getPlayer(), Flags.BUILD)) {
+
+			for (double y = 1; y <= pos1.world.getMaxHeight(); y++)
+				for (double x = xLow; x <= xHigh; x++)
+					for (double z = zLow; z <= zHigh; z++)
+						if (!WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery().testState(
+								BukkitAdapter.adapt(new Location(pos1.world, x, y, z)),
+								WorldGuardPlugin.inst().wrapPlayer(citizen.getPlayer()), Flags.BUILD))
 							return true;
-						}
-					}
-				}
-			}
-		} return false;
+		}
+		return false;
 	}
 }
